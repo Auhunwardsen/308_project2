fceee187caae668b09470612ebad38fd1d24e2ef
auhun47 <auhun47@linuxvdi-f25-50.ece.iastate.edu>
2026-05-04 00:59:40 -0500

    test runs

--- Proj02-starter/test_stages.sh
+++ Proj02-starter/test_stages.sh
@@ -14,21 +14,23 @@ send "DEL ghost"
 
 echo
 echo "=== Stage 2: parallel clients ==="
-for i in $(seq 1 20); do ( send "PUT k$i v$i"; send "GET k$i" ) & done
+for i in $(seq 1 10); do ( send "PUT k$i v$i"; send "GET k$i" ) & done
 wait
 send "STATS"
 
 echo
 echo "=== Stage 3: shared-key contention ==="
-for i in $(seq 1 50); do send "PUT shared$((i % 3)) v$i" & done
+for i in $(seq 1 25); do send "PUT shared$((i % 3)) v$i" & done
 wait
 send "GET shared0"
+wait
 send "GET shared1"
+wait
 send "GET shared2"
 
 echo
 echo "=== Stage 4: TTL sweeper ==="
-for i in $(seq 1 20); do send "PUT t$i v$i 1"; done
+for i in $(seq 1 10); do send "PUT t$i v$i 1"; done
 send "STATS"
 sleep 3
 send "STATS"
